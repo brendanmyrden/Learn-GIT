0099dfb74595c7fccd6c692fe355218e0bc852a7
brendanmyrden <brendan4myrden@proton.me>
2023-03-26 01:35:00 -0230

    Update to styles.ccs, app.s internalMessage_2

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 const internalMessage = `This is a working process of learning GIT, the connections therein and its relation to GitHub`;
 
 console.log(internalMessage);
+
+const internalMessage_2 = `Adapted through Manuel Pinto's CodePen, through Colt Steele's Youtube Channel`;
+
+console.log(internalMessage_2);
