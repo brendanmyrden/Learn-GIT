4896cc16d3cb2516bdb8a3688ff39a915a1a24f5
brendanmyrden <brendan4myrden@proton.me>
2023-03-26 01:59:24 -0230

    Update to app.js, styles.css

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const internalMessage = `This is a working process of learning GIT, the connecti
 
 console.log(internalMessage);
 
-const internalMessage_2 = `Adapted through Manuel Pinto's CodePen, through Colt Steele's Youtube Channel`;
+const internalMessage_2 = `Adapted through https://manuel.pinto.dev - Manuel Pinto's CodePen, through Colt Steele's - https://youtu.be/USjZcfj8yxE - Youtube Channel`;
 
 console.log(internalMessage_2);
